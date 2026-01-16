d4737928cad927e4a9a560172f8b0e7b4d9b8e55
Adelodun Peter <adelodunpeter69@gmail.com>
2026-01-17 00:24:58 +0100

    fix: use correct export names from react-resizable-panels

--- src/renderer/src/components/ui/resizable.tsx
+++ src/renderer/src/components/ui/resizable.tsx
@@ -1,14 +1,14 @@
 import { ComponentProps } from "react"
 import { GripVertical } from "lucide-react"
-import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels"
+import { Group, Panel, Separator } from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
 const ResizablePanelGroup = ({
   className,
   ...props
-}: ComponentProps<typeof PanelGroup>) => (
-  <PanelGroup
+}: ComponentProps<typeof Group>) => (
+  <Group
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
       className
@@ -23,10 +23,10 @@ const ResizableHandle = ({
   withHandle,
   className,
   ...props
-}: ComponentProps<typeof PanelResizeHandle> & {
+}: ComponentProps<typeof Separator> & {
   withHandle?: boolean
 }) => (
-  <PanelResizeHandle
+  <Separator
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
@@ -38,7 +38,7 @@ const ResizableHandle = ({
         <GripVertical className="h-2.5 w-2.5" />
       </div>
     )}
-  </PanelResizeHandle>
+  </Separator>
 )
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
