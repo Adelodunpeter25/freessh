580bf43d996d31c6e2a6ac521d84bc5cb1f10c3f
Adelodun Peter <adelodunpeter69@gmail.com>
2026-01-16 22:39:46 +0100

    fix: remove edit header from image preview

--- src/renderer/src/components/sftp/filepreview/ImagePreview.tsx
+++ src/renderer/src/components/sftp/filepreview/ImagePreview.tsx
@@ -1,5 +1,3 @@
-import { FilePreviewHeader } from './FilePreviewHeader'
-
 interface ImagePreviewProps {
   src: string
   filename: string
@@ -7,22 +5,12 @@ interface ImagePreviewProps {
 
 export function ImagePreview({ src, filename }: ImagePreviewProps) {
   return (
-    <div className="flex flex-col h-full">
-      <FilePreviewHeader
-        filename={filename}
-        isEditing={false}
-        isDirty={false}
-        onEdit={() => {}}
-        onSave={() => {}}
-        onCancel={() => {}}
+    <div className="flex-1 flex items-center justify-center p-4 bg-black/20">
+      <img 
+        src={src} 
+        alt={filename} 
+        className="max-w-full max-h-full object-contain"
       />
-      <div className="flex-1 flex items-center justify-center p-4 bg-black/20">
-        <img 
-          src={src} 
-          alt={filename} 
-          className="max-w-full max-h-full object-contain"
-        />
-      </div>
     </div>
   )
 }
