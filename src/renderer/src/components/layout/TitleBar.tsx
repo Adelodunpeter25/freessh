2dc5974aa32e90ea91b531dbe71c67c72d736f0a
Adelodun Peter <adelodunpeter69@gmail.com>
2026-01-23 18:39:08 +0100

    refactor: remove FreeSSH title from titlebar

--- src/renderer/src/components/layout/TitleBar.tsx
+++ src/renderer/src/components/layout/TitleBar.tsx
@@ -20,11 +20,7 @@ const noDrag = { WebkitAppRegion: 'no-drag' } as React.CSSProperties
 export function TitleBar({ showHome, showSFTP, showTerminal, sidebarOpen, onHomeClick, onSFTPClick, onSessionClick, onSidebarToggle }: TitleBarProps) {
   return (
     <div className="h-14 bg-background border-b border-border flex items-center px-4 select-none" style={{ WebkitAppRegion: 'drag' } as React.CSSProperties}>
-      <div className="flex items-center gap-3 pl-16">
-        <span className="text-sm font-semibold text-foreground">FreeSSH</span>
-      </div>
-      
-      <div className="flex-1 flex items-center ml-4">
+      <div className="flex-1 flex items-center pl-16">
         <SessionTabBar showHome={showHome} showSFTP={showSFTP} onHomeClick={onHomeClick} onSFTPClick={onSFTPClick} onSessionClick={onSessionClick} />
       </div>
 
