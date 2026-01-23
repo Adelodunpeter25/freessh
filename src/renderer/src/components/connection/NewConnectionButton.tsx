a62ccc8b357ee8ea7f0edc055fc859f81aafeaa5
Adelodun Peter <adelodunpeter69@gmail.com>
2026-01-23 13:37:00 +0100

    feat: add tooltip and rotate animation to add connection button

--- src/renderer/src/components/connection/NewConnectionButton.tsx
+++ src/renderer/src/components/connection/NewConnectionButton.tsx
@@ -1,5 +1,6 @@
 import { Plus } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 
 interface NewConnectionButtonProps {
   onClick: () => void
@@ -7,12 +8,19 @@ interface NewConnectionButtonProps {
 
 export function NewConnectionButton({ onClick }: NewConnectionButtonProps) {
   return (
-    <Button 
-      onClick={onClick} 
-      size="icon"
-      className="fixed bottom-6 right-6 h-14 w-14 rounded-full shadow-lg z-50"
-    >
-      <Plus className="h-6 w-6" />
-    </Button>
+    <TooltipProvider delayDuration={300}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button 
+            onClick={onClick} 
+            size="icon"
+            className="fixed bottom-6 right-6 h-14 w-14 rounded-full shadow-lg z-50 group"
+          >
+            <Plus className="h-6 w-6 transition-transform group-hover:rotate-90" />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>Add new connection</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
