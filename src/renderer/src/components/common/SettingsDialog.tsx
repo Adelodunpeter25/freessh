b7dfcbc7854cd9be27db880737202d30baf01fe4
Adelodun Peter <adelodunpeter69@gmail.com>
2026-01-23 20:32:21 +0100

    refactor: replace SettingsPage with SettingsDialog modal

---
+++ src/renderer/src/components/common/SettingsDialog.tsx
@@ -0,0 +1,23 @@
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { ThemeSettings } from '@/components/settings/ThemeSettings'
+
+interface SettingsDialogProps {
+  open: boolean
+  onOpenChange: (open: boolean) => void
+}
+
+export function SettingsDialog({ open, onOpenChange }: SettingsDialogProps) {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="max-w-2xl max-h-[80vh] overflow-y-auto">
+        <DialogHeader>
+          <DialogTitle>Settings</DialogTitle>
+        </DialogHeader>
+        
+        <div className="space-y-8 mt-4">
+          <ThemeSettings />
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
