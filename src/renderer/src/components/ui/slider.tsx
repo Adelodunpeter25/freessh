29af88eb183d6e52c2dd5e532e48e992beba893d
Adelodun Peter <adelodunpeter69@gmail.com>
2026-01-23 12:22:45 +0100

    fix: recreate slider with cleaner syntax

--- src/renderer/src/components/ui/slider.tsx
+++ src/renderer/src/components/ui/slider.tsx
@@ -1,7 +1,8 @@
-import { forwardRef, useState, ChangeEvent, InputHTMLAttributes } from "react"
+import { forwardRef, useState } from "react"
 import { cn } from "@/lib/utils"
 
-interface SliderProps extends InputHTMLAttributes<HTMLInputElement> {
+interface SliderProps {
+  className?: string
   defaultValue?: number[]
   value?: number[]
   onValueChange?: (value: number[]) => void
@@ -10,14 +11,14 @@ interface SliderProps extends InputHTMLAttributes<HTMLInputElement> {
   step?: number
 }
 
-const Slider = forwardRef(
-  ({ className, defaultValue, value, onValueChange, min = 0, max = 100, step = 1, ...props }: SliderProps, ref: any) => {
+export const Slider = forwardRef<HTMLInputElement, SliderProps>(
+  ({ className, defaultValue, value, onValueChange, min = 0, max = 100, step = 1 }, ref) => {
     const [internalValue, setInternalValue] = useState(defaultValue?.[0] || value?.[0] || 0)
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       const newValue = Number(e.target.value)
       setInternalValue(newValue)
-      onValueChange?([newValue])
+      onValueChange?.([newValue])
     }
 
     const currentValue = value?.[0] ?? internalValue
@@ -37,11 +38,9 @@ const Slider = forwardRef(
           "[&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-primary [&::-moz-range-thumb]:border-0",
           className
         )}
-        {...props}
       />
     )
   }
 )
-Slider.displayName = "Slider"
 
-export { Slider }
+Slider.displayName = "Slider"
