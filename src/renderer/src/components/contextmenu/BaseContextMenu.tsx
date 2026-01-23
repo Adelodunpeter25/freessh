3e52c1492bd1cfd684d9c818adabe5a068738084
Adelodun Peter <adelodunpeter69@gmail.com>
2026-01-23 13:20:55 +0100

    fix: add full height/width to ContextMenuTrigger

--- src/renderer/src/components/contextmenu/BaseContextMenu.tsx
+++ src/renderer/src/components/contextmenu/BaseContextMenu.tsx
@@ -24,7 +24,7 @@ interface BaseContextMenuProps {
 export function BaseContextMenu({ children, actions }: BaseContextMenuProps) {
   return (
     <ContextMenu>
-      <ContextMenuTrigger asChild>{children}</ContextMenuTrigger>
+      <ContextMenuTrigger asChild className="h-full w-full">{children}</ContextMenuTrigger>
       <ContextMenuContent>
         {actions.map((action, index) => (
           <div key={index}>
