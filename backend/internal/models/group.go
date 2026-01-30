64f7211c973a71454fa40bb06c9ae660fde91c7e
Adelodun Peter <adelodunpeter69@gmail.com>
2026-01-30 15:31:03 +0100

    Remove connection_count from groups.json storage (computed field)

--- backend/internal/models/group.go
+++ backend/internal/models/group.go
@@ -5,6 +5,6 @@ import "time"
 type Group struct {
 	ID              string    `json:"id"`
 	Name            string    `json:"name"`
-	ConnectionCount int       `json:"connection_count"`
+	ConnectionCount int       `json:"connection_count,omitempty"`
 	CreatedAt       time.Time `json:"created_at"`
 }
