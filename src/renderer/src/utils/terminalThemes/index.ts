48759f8e1036f29919430b07bd4b265a02fa2457
Adelodun Peter <adelodunpeter69@gmail.com>
2026-01-31 13:24:36 +0100

    Fix type export in terminalThemes index.ts

--- src/renderer/src/utils/terminalThemes/index.ts
+++ src/renderer/src/utils/terminalThemes/index.ts
@@ -1,8 +1,8 @@
-export { TerminalThemePreset } from './types'
-export { darkThemes } from './dark'
-export { lightThemes } from './light'
-
 import { darkThemes } from './dark'
 import { lightThemes } from './light'
+
+export type { TerminalThemePreset } from './types'
+export { darkThemes } from './dark'
+export { lightThemes } from './light'
 
 export const terminalThemePresets = [...darkThemes, ...lightThemes]
