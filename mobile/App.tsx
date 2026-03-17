39061c48b2e6df633dab22c70a9aa124549e60b7
Adelodun Peter <adelodunpeter69@gmail.com>
2026-03-17 13:01:20 +0100

    Respect system theme

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,18 @@
 import '@tamagui/native/setup-zeego'
 import { StatusBar } from 'expo-status-bar'
 import { useEffect } from 'react'
-import { Appearance } from 'react-native'
-import { TamaguiProvider, Theme } from 'tamagui'
+import { Appearance, useColorScheme } from 'react-native'
+import { TamaguiProvider } from 'tamagui'
 
 import { AppNavigator } from './src/navigation/AppNavigator'
 import { useThemeStore } from './src/stores'
 import tamaguiConfig from './tamagui.config'
 
 export default function App() {
+  const followSystem = useThemeStore((state) => state.followSystem)
   const theme = useThemeStore((state) => state.theme)
   const setSystemTheme = useThemeStore((state) => state.setSystemTheme)
+  const scheme = useColorScheme()
 
   useEffect(() => {
     const subscription = Appearance.addChangeListener(({ colorScheme }) => {
@@ -20,12 +22,16 @@ export default function App() {
     return () => subscription.remove()
   }, [setSystemTheme])
 
+  const resolvedTheme = followSystem
+    ? scheme === 'dark'
+      ? 'dark'
+      : 'light'
+    : theme
+
   return (
-    <TamaguiProvider config={tamaguiConfig} defaultTheme={theme}>
-      <Theme name={theme}>
-        <AppNavigator />
-        <StatusBar style={theme === 'dark' ? 'light' : 'dark'} />
-      </Theme>
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={resolvedTheme}>
+      <AppNavigator />
+      <StatusBar style={resolvedTheme === 'dark' ? 'light' : 'dark'} />
     </TamaguiProvider>
   )
 }
