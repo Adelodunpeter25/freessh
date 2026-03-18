51ccdcf83f0826d44cc511c616ce79eda0ca0ff9
Adelodun Peter <adelodunpeter69@gmail.com>
2026-03-18 12:25:10 +0100

    Remove config plugin, use expo-module.config.json only

--- mobile/modules/hardware-keyboard/app.plugin.js
+++
@@ -1,9 +0,0 @@
-const { withDangerousMod } = require("@expo/config-plugins");
-
-const withHardwareKeyboard = (config) => {
-  // This is a native module, no config plugin modifications needed
-  // The expo-module.config.json handles the native module registration
-  return config;
-};
-
-module.exports = withHardwareKeyboard;
