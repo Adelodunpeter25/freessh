7f076360dc72dc567876faaa1e7bc38761de60a0
Adelodun Peter <adelodunpeter69@gmail.com>
2026-03-18 12:31:49 +0100

    Add modules path alias to babel and tsconfig

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -16,6 +16,7 @@ module.exports = function (api) {
             '@/types': './src/types',
             '@/utils': './src/utils',
             '@/navigation': './src/navigation',
+            '@/modules': './modules',
           },
         },
       ],
